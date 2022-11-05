d21cf339663c419c3b4a87abbc6a99e2fbacf0a4
irina.maksimova <irina.maksimova@xperience.ai>
2022-11-05 22:04:53 +0300

    loaimpl get_alternatives for app.py

--- app.py
+++ app.py
@@ -12,18 +12,16 @@ CORS(app)
 
 @app.route('/get_alternatives')
 def get_alternatives():
-    name = request.args.get('name', '')
-
-    # pass the name to data analysis script
-
-    return sustainable_alternatives.get_alternatives(name)
+    id = request.args.get('id', '')
+    
+    return sustainable_alternatives.get_alternatives(id)
 
 
 @app.route('/get_info')
 def get_info():
-    name = request.args.get('name', '')
+    id = request.args.get('id', '')
 
-    return sustainable_alternatives.get_info(name)
+    return sustainable_alternatives.get_info(id)
 
 
 if __name__ == '__main__':
