f6e18a2aea353e4cc67214ffe0957358117fd826
irina.maksimova <irina.maksimova@xperience.ai>
2022-11-06 01:55:38 +0300

    updated material_data calculation & added d con- pro- feaches

--- sustainable_alternatives.py
+++ sustainable_alternatives.py
@@ -15,27 +15,26 @@ def get_sheet_data():
 
 
 def get_material_data():
-    material_data = pd.read_csv("data/material.csv", sep=";")[["Material", "Embodied Energy", "Carbon Footprint", "Water Usage"]]
+    material_data = pd.read_csv("data/material.csv", sep=";")[["Material", "Embodied Energy", "Carbon Footprint", "Water Usage", "Pro 1", "Pro 2", "Con 1","Con 2", "kg/pc"]]
     material_data.at[6, "Material"] = "Galvanized steel"
 
+    material_data['kg/pc'] = pd.to_numeric(material_data['kg/pc'].map(lambda x: x.replace(",", ".")))
     material_data['Embodied Energy'] = pd.to_numeric(material_data['Embodied Energy'].map(lambda x: x.replace(",", ".")))
-    Embodied_Energy = material_data['Embodied Energy']
-    energy_item_size = (Embodied_Energy.max() - Embodied_Energy.min()) / 5
-    Embodied_Energy = (Embodied_Energy - Embodied_Energy.min()) / (Embodied_Energy.max() - Embodied_Energy.min())
-
     material_data['Carbon Footprint'] = pd.to_numeric(material_data['Carbon Footprint'].map(lambda x: x.replace(",", ".")))
-    Carbon_Footprint = material_data['Carbon Footprint']
-    carbon_footprint_item_size = (Carbon_Footprint.max() - Carbon_Footprint.min()) / 5
-    Carbon_Footprint = (Carbon_Footprint - Carbon_Footprint.min()) / (Carbon_Footprint.max() - Embodied_Energy.min())
-
     material_data['Water Usage'] = pd.to_numeric(material_data['Water Usage'].map(lambda x: x.replace(",", ".")))
+        
+    material_data["n_home"]    = (material_data['Embodied Energy'] * material_data['kg/pc'] * 0.278) / 1500
+    material_data["n_trees"]   = (material_data['Carbon Footprint'] * material_data['kg/pc']) / 25.
+    material_data["n_bottles"] = (material_data['Water Usage'] * material_data['kg/pc'])
+    
+    Embodied_Energy = material_data['Embodied Energy']
+    Carbon_Footprint = material_data['Carbon Footprint']
     Water_Usage = material_data['Water Usage']
-    Water_Usage = material_data['Water Usage']
-    water_usage_item_size = (Water_Usage.max() - Water_Usage.min()) / 5
+    Embodied_Energy = (Embodied_Energy - Embodied_Energy.min()) / (Embodied_Energy.max() - Embodied_Energy.min())
+    Carbon_Footprint = (Carbon_Footprint - Carbon_Footprint.min()) / (Carbon_Footprint.max() - Embodied_Energy.min())
     Water_Usage = (Water_Usage - Water_Usage.min()) / (Water_Usage.max() - Water_Usage.min())
-
     material_data['Score'] = 1 - (Embodied_Energy + Carbon_Footprint + Water_Usage) / 3
-    return material_data, energy_item_size, carbon_footprint_item_size, water_usage_item_size
+    return material_data
 
 def get_product_code_2_description(sheets_data):
     important_columns = ["Code", "Class", "Name", "Weight", "WeightUnit", "Length", "Height", "Width", "Thickness", "Diameter", "ShortDescription"]
@@ -52,16 +51,12 @@ def get_product_code_2_description(sheets_data):
 
 def get_alternatives(product_code, top_k=3):    
     sheets_data = get_sheet_data()
-    # print("Here:", len(sheets_data))
-    # raise
     product_code_2_description = get_product_code_2_description(sheets_data)
-    material_data, energy_item_size, carbon_footprint_item_size, water_usage_item_size = get_material_data()
+    material_data = get_material_data()
     
     model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
     src_desc = product_code_2_description[product_code]
     
-    # print("Here:", len(product_code_2_description))
-    # raise
     src_material = sheets_data[sheets_data["Code"] == product_code].Material.to_list()[0]
     df = pd.DataFrame(columns=["Code", "cos", "eff_score", "material"]) 
     for code in product_code_2_description:
@@ -90,14 +85,20 @@ def get_alternatives(product_code, top_k=3):
     urls = [f"https://www.cronvall.fi/epages/CronvallShop.sf/sec89130f6a70/?ObjectPath=/Shops/CronvallShop/Products/{c}" for c in df_top_k.Code]
     prices = [sheets_data[sheets_data["Code"] == c].Price.to_list()[0].split(".")[0] for c in df_top_k.Code]
     names = [sheets_data[sheets_data["Code"] == c].Name.to_list()[0] for c in df_top_k.Code]
-    trees_differs = [math.ceil(material_data[material_data["Material"] == m]["Carbon Footprint"].to_list()[0] /  carbon_footprint_item_size) for m in df_top_k.material]
-    trees_differs = [i - trees_differs[0] for i in trees_differs] # If > 0 - RED
-    battle_differs = [math.ceil(material_data[material_data["Material"] == m]["Water Usage"].to_list()[0] /  water_usage_item_size) for m in df_top_k.material]
-    battle_differs = [i - battle_differs[0] for i in battle_differs] # If > 0 - RED
-    energy_differs = [math.ceil(material_data[material_data["Material"] == m]["Embodied Energy"].to_list()[0] /  energy_item_size) for m in df_top_k.material]
-    energy_differs = [i - energy_differs[0] for i in energy_differs] # If > 0 - RED
-    energy_differs = [i - energy_differs[0] for i in energy_differs] # If > 0 - RED
+    
+    trees_differs =  [material_data[material_data["Material"] == m]["n_trees"].to_list()[0]   for m in df_top_k.material]
+    battle_differs = [material_data[material_data["Material"] == m]["n_bottles"].to_list()[0] for m in df_top_k.material]
+    energy_differs = [material_data[material_data["Material"] == m]["n_home"].to_list()[0]    for m in df_top_k.material]
+    
+    trees_differs =  [i - trees_differs[0]  for i in trees_differs] 
+    battle_differs = [i - battle_differs[0] for i in battle_differs]
+    energy_differs = [i - energy_differs[0] for i in energy_differs]
+    
     co2eff = [material_data[material_data["Material"] == m]["Carbon Footprint"].to_list()[0] for m in df_top_k.material]
+    pro_1 = [material_data[material_data["Material"] == m]["Pro 1"].to_list()[0] for m in df_top_k.material]
+    pro_2 = [material_data[material_data["Material"] == m]["Pro 2"].to_list()[0] for m in df_top_k.material]
+    con_1 = [material_data[material_data["Material"] == m]["Con 1"].to_list()[0] for m in df_top_k.material]
+    con_2 = [material_data[material_data["Material"] == m]["Con 1"].to_list()[0] for m in df_top_k.material]
 
     df_top_k["Images"] = images
     df_top_k["Urls"] = urls
@@ -108,6 +109,10 @@ def get_alternatives(product_code, top_k=3):
     df_top_k["Names"] = names
     df_top_k["CO2eff"] = co2eff
     df_top_k["avg_eff_score"] = [material_data.Score.mean(), ] * len(co2eff)
+    df_top_k["pro_1"] = pro_1
+    df_top_k["pro_2"] = pro_2
+    df_top_k["con_1"] = con_1
+    df_top_k["con_2"] = con_2
     return df_top_k.to_json(orient='records')
 
 
